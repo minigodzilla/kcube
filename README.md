23cdee5fc053cad7cafd3b67ff61a3402284e443
Steve Diabo <stevediabo@gmail.com>
2020-10-26 13:32:21 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# HTML Boilerplate
+
+## Getting Started
+
+Below instructions will help you get a copy of the project up and running on your local machine for starting website development.
+
+### Prerequisites
+
+Install gulp globally on your system
+
+```
+npm install gulp -g
+```
+
+### Cloning
+
+Clone project from github Repository
+
+```
+```
+
+### Installing
+
+Then Install the packages included in *package.json*
+
+```
+$ npm install
+```
+
+### Build
+
+Build for generating pure css/js/fonts/html files in public folder, which is the one to deploy
+
+```
+$ npm run build
+```
+
+### Run the project - Live on browser
+
+For live development and run the project in browser
+
+```
+$ npm run start
+```
+
+## Built With
+
+* [Gulp 4](https://gulpjs.com/)
+* [Sass](https://sass-lang.com/)
+
---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+const gulp = require('gulp');
+const gulpIf = require('gulp-if');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const htmlmin = require('gulp-htmlmin');
+const cssmin = require('gulp-cssmin');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const jsImport = require('gulp-js-import');
+const sourcemaps = require('gulp-sourcemaps');
+const clean = require('gulp-clean');
+const cssbeautify = require('gulp-cssbeautify');
+const htmlbeautify = require('gulp-html-beautify')
+const isProd = process.env.NODE_ENV === 'prod';
+
+var options = { };
+
+const htmlFile = [
+    'src/*.html'
+]
+
+function html() {
+    return gulp.src(htmlFile)
+        .pipe(htmlbeautify())
+        .pipe(gulpIf(isProd, htmlmin({
+            collapseWhitespace: true
+        })))
+        .pipe(gulp.dest('public'));
+}
+
+function css() {
+    return gulp.src('src/assets/sass/style.scss')
+        .pipe(gulpIf(!isProd, sourcemaps.init()))
+        .pipe(sass({
+            includePaths: ['node_modules']
+        }).on('error', sass.logError))
+        .pipe(cssbeautify({
+            indent: '  ',
+            openbrace: 'separate-line',
+            autosemicolon: true
+        }))
+        .pipe(gulpIf(!isProd, sourcemaps.write()))
+        .pipe(gulpIf(isProd, cssmin()))
+        .pipe(gulp.dest('public/assets/css/'));
+}
+
+function js() {
+    return gulp.src('src/assets/js/*.js')
+        .pipe(jsImport({
+            hideConsole: true
+        }))
+        // .pipe(concat('all.js'))
+        .pipe(gulpIf(isProd, uglify()))
+        .pipe(gulp.dest('public/assets/js'));
+}
+
+function img() {
+    return gulp.src('src/assets/img/*')
+        .pipe(gulpIf(isProd, imagemin()))
+        .pipe(gulp.dest('public/assets/img/'));
+}
+
+function serve() {
+    browserSync.init({
+        open: true,
+        notify: false,
+        server: './public'
+    });
+}
+
+function browserSyncReload(done) {
+    browserSync.reload();
+    done();
+}
+
+
+function watchFiles() {
+    gulp.watch('src/**/*.html', gulp.series(html, browserSyncReload));
+    gulp.watch('src/assets/**/*.scss', gulp.series(css, browserSyncReload));
+    gulp.watch('src/assets/**/*.js', gulp.series(js, browserSyncReload));
+    gulp.watch('src/assets/img/**/*.*', gulp.series(img));
+
+    return;
+}
+
+function del() {
+    return gulp.src('public/*', {read: false})
+        .pipe(clean());
+}
+
+exports.css = css;
+exports.html = html;
+exports.js = js;
+exports.del = del;
+
+// this is the serve 
+exports.default = gulp.parallel(html, css, js, img, watchFiles, serve);
+
+// this is the build task
+exports.build = gulp.series(del, html, css, js, img);
